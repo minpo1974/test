542e4b90889ff943fee08a032ae2d9c7cb50c0f2
minpo1974 <minpos@gmail.com>
2023-05-11 14:42:24 +0900

    first commit

---
+++ xgboost.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
